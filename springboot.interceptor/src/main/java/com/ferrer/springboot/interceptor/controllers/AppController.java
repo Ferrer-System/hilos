7fa370b31b23eae3d2580ead8734259578db0e6f
Ferrer <ortizdionicio993@gmail.com>
2025-02-13 18:14:26 -0600

    Subiendo proyecto Spring Boot LoadingTimeInterceptor

---
+++ springboot.interceptor/src/main/java/com/ferrer/springboot/interceptor/controllers/AppController.java
@@ -0,0 +1,36 @@
+package com.ferrer.springboot.interceptor.controllers;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+	@GetMapping("/foo")
+	public Map<String, String> foo(){
+		
+		
+		return Collections.singletonMap("message", "Handler foo del controlador");
+	}
+	
+	
+	@GetMapping("/bar")
+	public Map<String, String> bar(){
+		
+		
+		return Collections.singletonMap("message", "Handler bar del controlador");
+	}
+	
+	@GetMapping("/baz")
+	public Map<String, String> baz(){
+		
+		
+		return Collections.singletonMap("message", "Handler baz del controlador");
+	}
+	
+}
