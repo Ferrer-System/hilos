58b729e2bce64a305b8af135315bd4be3f14b4df
Ferrer <ortizdionicio993@gmail.com>
2025-03-18 17:20:56 -0600

    Eliminar proyectos que fueron subidos a una ubicacion incorrecta

--- springboot.interceptor/spring.noot.aop/src/main/java/com/ferrer/spring/boot/aop/services/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package com.ferrer.spring.boot.aop.services;
-
-public interface GreetingService {
-
-	String sayHello(String person, String phrase);
-	String sayHelloError(String person, String phrase);
-
-	
-}
